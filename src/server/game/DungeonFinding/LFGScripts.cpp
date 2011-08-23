427da22cfad7e060094f7e670eda4a07b061a8c9
Eze <king@pin.kin>
2011-08-23 20:00:16 +0200

    Ajout du Déserteur pour le donjon aléa

--- src/server/game/DungeonFinding/LFGScripts.cpp
+++ src/server/game/DungeonFinding/LFGScripts.cpp
@@ -84,6 +84,9 @@ void LFGScripts::OnRemoveMember(Group* group, uint64 guid, RemoveMethod& method,
     sLFGMgr->ClearState(guid);
     if (Player *plr = sObjectMgr->GetPlayer(guid))
     {
+	// Add deserter flag
+	if (sLFGMgr->GetState(gguid) != LFG_STATE_FINISHED_DUNGEON && method == GROUP_REMOVEMETHOD_LEAVE)
+		plr->CastSpell(plr, LFG_SPELL_DUNGEON_DESERTER, true);
         /*
         if (method == GROUP_REMOVEMETHOD_LEAVE)
             // Add deserter flag
