5db32dab378f92b42ad26c9c132c8b29c193ae3f
s@m <sammassam@yahoo.fr>
2011-12-29 15:43:22 +0100

    test repa repo

--- test2.sh
+++ test2.sh
@@ -1,40 +1,13 @@
 #!/bin/sh
 
-git filter-branch --env-filter '
-
-an="$GIT_AUTHOR_NAME"
-am="$GIT_AUTHOR_EMAIL"
-cn="$GIT_COMMITTER_NAME"
-cm="$GIT_COMMITTER_EMAIL"
-
-if [ "$GIT_COMMIT" = "16565316e4a177d152a64a13db14e7c0c8335c2b" ]
+git filter-branch -f --env-filter '
+if [ "$GIT_AUTHOR_NAME" = "KingPin" ]
 then 
-	echo "trouve"
-	echo "$GIT_AUTHOR_NAME"
-	echo "$GIT_COMMITTER_NAME"
-fi
-if [ "$GIT_COMMIT" = "0d4bbd96410dfa6d9d9bf761f26ba7651da38a96" ]
-then 
-	echo "trouve bis"
-	echo "$GIT_AUTHOR_NAME"
-	echo "$GIT_COMMITTER_NAME"
-fi
-if [ "$GIT_AUTHOR_NAME" = "malcromdev@gmail.com" ]
-then
-    cn="malcromdev"
-    cm="malcromdev@gmail.com"
-    an="malcromdev"
-    am="malcromdev@gmail.com"
-fi
-if [ "$GIT_COMMITTER_NAME" = "malcromdev@gmail.com" ]
-then
-    cn="malcromdev"
-    cm="malcromdev@gmail.com"
-    an="malcromdev"
-    am="malcromdev@gmail.com"
-fi
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+	echo "trouvé"
+	GIT_COMMITTER_NAME="test" &&
+       GIT_AUTHOR_NAME="test" &&
+       GIT_COMMITTER_EMAIL="test@test.te"&&
+       GIT_AUTHOR_EMAIL="test@test.te"&&
+	export GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL &&
+     	export GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL
+fi' -- --all
