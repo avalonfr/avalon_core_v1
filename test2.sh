17e72134ed9c2fcfb5787ad6a28ea647617e9d33
s@m <sammassam@yahoo.fr>
2011-12-29 16:25:34 +0100

    test

--- test2.sh
+++ test2.sh
@@ -3,11 +3,17 @@
 git filter-branch -f --env-filter '
 if [ "$GIT_AUTHOR_NAME" = "KingPin" ]
 then 
+	an="test"
+	am="test@test.te"
+	cn="test"
+	cm="test@test.te"
 	echo "trouvé"
-	GIT_COMMITTER_NAME="test" &&
-       GIT_AUTHOR_NAME="test" &&
-       GIT_COMMITTER_EMAIL="test@test.te"&&
-       GIT_AUTHOR_EMAIL="test@test.te"&&
-	export GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL &&
-     	export GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL
+	GIT_COMMITTER_NAME=$cn &&
+    GIT_AUTHOR_NAME=$an &&
+    GIT_COMMITTER_EMAIL=$cm &&
+    GIT_AUTHOR_EMAIL=$am &&
+	export GIT_AUTHOR_NAME &&
+	export GIT_AUTHOR_EMAIL &&
+    export GIT_COMMITTER_NAME &&
+	export GIT_COMMITTER_EMAIL
 fi' -- --all
